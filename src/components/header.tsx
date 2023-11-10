ad3c2cb5e638994ab7d6f132a60dcbbef0c63147
John Winsor <johnwinsor@gmail.com>
2023-11-09 16:49:12 -0800

    hours

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import Image from 'next/image'
 import Link from 'next/link'
-import { Flex, Heading, Text, Container} from '@radix-ui/themes'
+import { Flex } from '@radix-ui/themes'
 import olinLogo from '../../public/oaklandLibrary.png'
 import '../app/globals.css'
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import classNames from 'classnames';
 import { ChatBubbleIcon } from '@radix-ui/react-icons'
-import { CaretDownIcon } from '@radix-ui/react-icons';
+import { CaretDownIcon } from '@radix-ui/react-icons'
+import { Hours } from "@/components/hours";
 
 export default function Header() {
   return (
@@ -26,7 +26,7 @@ export default function Header() {
                   />
               </Link>
             </Flex>
-            <ChatBubbleIcon />
+            <Hours />
           </Flex>
         </Flex>
         <Flex gap="5" className="text-sm md:text-base">
@@ -41,25 +41,26 @@ export default function Header() {
               <li style={{ gridRow: 'span 3' }}>
                 <NavigationMenu.Link asChild>
                   <a className="Callout" href="/">
-                    <svg aria-hidden width="38" height="38" viewBox="0 0 25 25" fill="white">
+                    {/* <svg aria-hidden width="38" height="38" viewBox="0 0 25 25" fill="white">
                       <path d="M12 25C7.58173 25 4 21.4183 4 17C4 12.5817 7.58173 9 12 9V25Z"></path>
                       <path d="M12 0H4V8H12V0Z"></path>
                       <path d="M17 8C19.2091 8 21 6.20914 21 4C21 1.79086 19.2091 0 17 0C14.7909 0 13 1.79086 13 4C13 6.20914 14.7909 8 17 8Z"></path>
-                    </svg>
-                    <div className="CalloutHeading">Radix Primitives</div>
-                    <p className="CalloutText">Unstyled, accessible components for React.</p>
+                    </svg> */}
+                    <svg width="38" height="38" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7.07926 0.222253C7.31275 -0.007434 7.6873 -0.007434 7.92079 0.222253L14.6708 6.86227C14.907 7.09465 14.9101 7.47453 14.6778 7.71076C14.4454 7.947 14.0655 7.95012 13.8293 7.71773L13 6.90201V12.5C13 12.7761 12.7762 13 12.5 13H2.50002C2.22388 13 2.00002 12.7761 2.00002 12.5V6.90201L1.17079 7.71773C0.934558 7.95012 0.554672 7.947 0.32229 7.71076C0.0899079 7.47453 0.0930283 7.09465 0.32926 6.86227L7.07926 0.222253ZM7.50002 1.49163L12 5.91831V12H10V8.49999C10 8.22385 9.77617 7.99999 9.50002 7.99999H6.50002C6.22388 7.99999 6.00002 8.22385 6.00002 8.49999V12H3.00002V5.91831L7.50002 1.49163ZM7.00002 12H9.00002V8.99999H7.00002V12Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+                    <div className="CalloutHeading">Library Information</div>
+                    <p className="CalloutText">Oakland campus-specific library policies, services and other information.</p>
                   </a>
                 </NavigationMenu.Link>
               </li>
 
-              <ListItem href="https://stitches.dev/" title="Stitches">
-                CSS-in-JS with best-in-class developer experience.
+              <ListItem href="https://library.northeastern.edu/oakland_campus/about-the-f-w-olin-library/policies-services/" title="Policies and Services">
+                Access policies, Borrowing, Purchase Requests, and Interlibrary Loan.
               </ListItem>
-              <ListItem href="/colors" title="Colors">
-                Beautiful, thought-out palettes with auto dark mode.
+              <ListItem href="https://library.mills.edu/forms/questions/libanswers-embed.php" title="Questions & Feedback">
+                Use this form to submit a new question or give us feedback.
               </ListItem>
-              <ListItem href="https://icons.radix-ui.com/" title="Icons">
-                A crisp set of 15x15 icons, balanced and consistent.
+              <ListItem href="https://library.northeastern.edu/administration/library-staff-directory/?ul_filter_department=F.%20W.%20Olin%20Library" title="Staff Directory">
+                Library staff at the F.W. Olin Library on the Oakland campus.
               </ListItem>
             </ul>
           </NavigationMenu.Content>
@@ -71,30 +72,30 @@ export default function Header() {
           </NavigationMenu.Trigger>
           <NavigationMenu.Content className="NavigationMenuContent">
             <ul className="List two">
-              <ListItem title="Introduction" href="/primitives/docs/overview/introduction">
-                Build high-quality, accessible design systems and web apps.
+              <ListItem title="Circulation & Borrowing" href="https://library.northeastern.edu/oakland_campus/about-the-f-w-olin-library/borrowing-from-the-f-w-olin-library/">
+                Where can I get library items? How Long Can I Have Library Items?
               </ListItem>
-              <ListItem title="Getting started" href="/primitives/docs/overview/getting-started">
-                A quick tutorial to get you up and running with Radix Primitives.
+              <ListItem title="Copyright Policy" href="https://library.northeastern.edu/research_instruction/theses-dissertations/what-is-fair-use/">
+                What is Fair Use and when permission is required for use of a copyrighted work?
               </ListItem>
-              <ListItem title="Styling" href="/primitives/docs/guides/styling">
-                Unstyled and compatible with any styling solution.
+              <ListItem title="Interlibrary Loans" href="https://library.northeastern.edu/ideas/interlibrary-loan/">
+                About our ILL services. Signing up for an ILLiad account. Requesting items from ILL and having them delivered to the Oakland campus.
               </ListItem>
-              <ListItem title="Animation" href="/primitives/docs/guides/animation">
-                Use CSS keyframes or any animation library of your choice.
+              <ListItem title="Course Reserves" href="https://library.mills.edu/reserves/">
+                Place physical libray materials or electronic resources on reserve for your students.
               </ListItem>
-              <ListItem title="Accessibility" href="/primitives/docs/overview/accessibility">
-                Tested in a range of browsers and assistive technologies.
+              <ListItem title="Departmental Liaisons" href="https://library.mills.edu/liaisons/">
+                Subject specialist librarians assigned to your departmenmt on the Oakland campus.
               </ListItem>
-              <ListItem title="Releases" href="/primitives/docs/overview/releases">
-                Radix Primitives releases and their changelogs.
+              <ListItem title="Purchase Requests" href="https://library.northeastern.edu/digital_production/recommend-a-purchase/">
+                Suggest a book, journal, or license for an electronic resource for purchase by the Library.
               </ListItem>
             </ul>
           </NavigationMenu.Content>
         </NavigationMenu.Item>
 
         <NavigationMenu.Item>
-          <NavigationMenu.Link className="NavigationMenuLink" href="https://github.com/radix-ui">
+          <NavigationMenu.Link className="NavigationMenuLink" href="/contact">
             Contact Us
           </NavigationMenu.Link>
         </NavigationMenu.Item>
@@ -129,7 +130,7 @@ const ListItem = React.forwardRef(({ className, children, title, ...props }, for
   <li>
     <NavigationMenu.Link asChild>
       <a className={classNames('ListItemLink', className)} {...props} ref={forwardedRef}>
-        <div className="ListItemHeading">{title}</div>
+        <div className="ListItemHeading"><Flex>{title}</Flex></div>
         <p className="ListItemText">{children}</p>
       </a>
     </NavigationMenu.Link>
---
+++ src/components/hours.jsx
@@ -0,0 +1,57 @@
+import Image from 'next/image'
+import { FaRegClock } from "react-icons/fa";
+
+const today = new Date()
+const tomorrow = new Date(today)
+tomorrow.setDate(tomorrow.getDate() + 1)
+
+async function getLibraryHours() {
+  const res = await fetch('https://www.googleapis.com/calendar/v3/calendars/c_b1631bcb3580a6cd93875bf2848dad1ea09747e99edcd4cc778301e7bb47a0ec@group.calendar.google.com/events?key=AIzaSyDGL-YEYrMZg_2pI-7jRsPvGwY5KSjuqVA' + '&maxResults=1' + '&timeMin=' + today.toISOString() + '&timeMax=' + tomorrow.toISOString() + '&orderBy=startTime' + '&singleEvents=True', {
+        method: 'GET',
+    });
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+async function getScHours() {
+  const res = await fetch('https://www.googleapis.com/calendar/v3/calendars/c_b1631bcb3580a6cd93875bf2848dad1ea09747e99edcd4cc778301e7bb47a0ec@group.calendar.google.com/events?key=AIzaSyDGL-YEYrMZg_2pI-7jRsPvGwY5KSjuqVA' + '&maxResults=1' + '&timeMin=' + today.toISOString() + '&timeMax=' + tomorrow.toISOString() + '&orderBy=startTime' + '&singleEvents=True', {
+        method: 'GET',
+    });
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+
+export async function Hours() {
+    const libraryData = await getLibraryHours()
+    const scData = await getScHours()
+
+    return (
+      <div className="justify-self-end mr-4">
+			  <div className="grid grid-cols-1 gap-0">
+          <div className='flex justify-end'>
+            <div className='pr-5 align-middle'>
+              <p className='pt-1'>
+                <FaRegClock className="text-red-900 h-4 w-4 md:h-6 md:w-6" />
+              </p>    
+            </div>
+            <div>
+              <p className="text-sm md:text-lg font-semibold underline text-end">Today's Hours</p>
+            </div>
+          </div>
+          <div className="col-span-2"><p className='md:text-sm text-xs text-end'>Library: {libraryData.items[0].summary}</p></div>
+          <div className="col-span-2"><p className='md:text-sm text-xs text-end'>Special Collections: {scData.items[0].summary}</p></div>
+        </div>
+      </div>
+    )
+  }
