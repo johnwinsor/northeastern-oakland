d4751d890fb6eb7952f0ef7f20fdb2e891fd7ff4
John Winsor <johnwinsor@gmail.com>
2023-11-14 11:58:57 -0800

    added sharp

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -29,27 +29,11 @@ const Search = () => (
             </div>
         </Tabs.Content>
         <Tabs.Content className="TabsContent" value="tab2">
-            <p className="Text">Change your password here. After saving, you'll be logged out.</p>
             <fieldset className="Fieldset">
-                <label className="Label" htmlFor="currentPassword">
-                    Current password
-                </label>
-                 <input className="Input" id="currentPassword" type="password" />
-            </fieldset>
-            <fieldset className="Fieldset">
-                <label className="Label" htmlFor="newPassword">
-                    New password
-                </label>
-                <input className="Input" id="newPassword" type="password" />
-            </fieldset>
-            <fieldset className="Fieldset">
-                <label className="Label" htmlFor="confirmPassword">
-                    Confirm password
-                </label>
-                <input className="Input" id="confirmPassword" type="password" />
+                <input className="Input" id="name" defaultValue="Search..." />
             </fieldset>
             <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-                <button className="Button green">Change password</button>
+                <button className="Button green">Search</button>
             </div>
         </Tabs.Content>
     </Tabs.Root>
