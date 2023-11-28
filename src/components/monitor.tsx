fe6f29bac9d4a4791728352309ca3bc43439fedb
John Winsor <johnwinsor@gmail.com>
2023-11-28 08:33:21 -0800

    appspace monitor full size image grid

--- src/components/monitor.tsx
+++ src/components/monitor.tsx
@@ -10,10 +10,10 @@ export const EmblaCarousel = ({ data }: {data: any}) => {
     const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
     return (
             <div className="parentgrid">
-                <Flex direction="column" className="p-5 text-slate-50 div1">
+                <Flex direction="column" className="p-5 text-slate-50" id="item-0">
                     <Text as="p" className="text-center text-2xl">New Arrivals</Text>
                 </Flex>
-                <div className='overflow-hidden h-full div2' ref={emblaRef}>
+                <div className='overflow-hidden' ref={emblaRef} id="item-1">
                     
                     <div className="embla__container">
                             {data.map((slide: any, index: any) => {
@@ -28,22 +28,25 @@ export const EmblaCarousel = ({ data }: {data: any}) => {
                                 const authorArray = slide.author.split(",")
                                 return (             
                                     <div className="embla__slide justify-center" key={index}>
-                                        <Flex height="100%" direction="column" className="items-center">
-                                            <Image
-                                                src={slide.coverurl}
-                                                width={400}
-                                                height={400}
-                                                alt='cover image'
-                                                className='boxshadow rounded-lg p-2 bg-slate-50 h-4/5 w-auto'
-                                                loading="lazy"
-                                            />
-                                            <Flex height="auto" direction="column">
-                                                <Text as="p" className="text-center text-2xl text-slate-50 w-96">{ splitTitle }</Text>
+
+                                            <div id="image" className="relative rounded-lg h-full p-2 border-2 bg-slate-50">
+                                                <Image
+                                                    src={slide.coverurl}
+                                                    width="0"
+                                                    height="0"
+                                                    sizes="100vw"
+                                                    alt='cover image'
+                                                    className='boxshadow w-auto h-full max-w-fit'
+                                                    loading="lazy"
+                                                />
+                                            </div>
+                                            <Flex grow="1" direction="column" id="bib" className="max-w-full justify-center">
+                                                <Text as="p" className="text-center text-2xl text-slate-50 justify-center max-w-md">{ splitTitle }</Text>
                                                 <Text as="p" className="text-center text-lg text-slate-50">{authorArray[1]} {authorArray[0]}</Text>
                                                 <Text as="p" className="text-xs md:text-sm lg-text-lg text-center text-slate-50">{callnoStatus}</Text>
                                                 <Text as="p" className="text-xs md:text-sm lg-text-lg text-center text-slate-50">Received: {slide.recDate}</Text>
                                             </Flex>
-                                        </Flex>
+
                                         
                                     </div>
                                 );
