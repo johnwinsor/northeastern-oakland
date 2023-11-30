804c24d0a0e2624cf94ed85bbc1a2ecca074009f
John Winsor <johnwinsor@gmail.com>
2023-11-29 23:06:35 -0800

    newbooks carousel done

--- src/components/newbooks.tsx
+++ src/components/newbooks.tsx
@@ -4,12 +4,13 @@ import Image from 'next/image'
 import { Flex, Text, Heading } from '@radix-ui/themes';
 import * as HoverCard from '@radix-ui/react-hover-card';
 import Link from 'next/link';
-import useEmblaCarousel from 'embla-carousel-react'
+import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 import '@/components/css/newbooks.css';
 
 export const Newbooks = ({ data }: {data: any}) => {
-    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 3000, stopOnMouseEnter: true, stopOnInteraction: false }),])
+    // const options = { delay: 3000, stopOnMouseEnter: true, stopOnInteraction: false }
+    const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay({ delay: 3000, stopOnMouseEnter: true, stopOnInteraction: false })])
     return (
         <Flex direction="column" gap="1" className="text-slate-50 min-h-[calc(100vh-242px)]">
             <Flex direction="column" className="h-12 p-5 text-slate-700">
@@ -17,7 +18,7 @@ export const Newbooks = ({ data }: {data: any}) => {
             </Flex>
             <Flex grow="1" className="pt-5">
                 <div className="embla" ref={emblaRef}>
-                    <div className="embla__container h-full">
+                    <div className="embla__container h-full grid grid-flow-col auto-cols-100 md:auto-cols-33 lg:auto-cols-20 2xl:auto-cols-14">
                         {data.map((src: any, i: any) => {
                             let callnoStatus = src.callNo.replace(/Unknown/g, "In Processing");
                             if (src.location == "On order") {
@@ -31,33 +32,30 @@ export const Newbooks = ({ data }: {data: any}) => {
                             const splitTitle = src.title.split(":")
                             const authorArray = src.author.split(",")
                             return (
-                                <Flex justify="center" gap="2" grow="1" direction="column" className="embla__slide flex-[0_0_100%] md:flex-[0_0_33%] xl:flex-[0_0_20%] px-2" key={i}>
-                                    <Flex justify="center" grow="1">
-                                        <HoverCard.Root>
-                                            <HoverCard.Trigger asChild>
-                                                <div className="relative">
-                                                    <Image
-                                                        src={src.coverurl}
-                                                        width="0"
-                                                        height="0"
-                                                        sizes="100vw"
-                                                        className="ImageTrigger p-2 border-2 border-slate-300 bg-slate-50 h-full w-full"
-                                                        alt="alt"
-                                                    />
-                                                </div>
-                                            </HoverCard.Trigger>
-                                            <HoverCard.Portal>
-                                                <HoverCard.Content className="HoverCardContent text-xs md:text-sm overflow-hidden overflow-y-auto" sideOffset={5} side="left">
-                                                    <Flex grow="1" direction="column" gap="3">
-                                                        <Text color="ruby" className="text-lg">{splitTitle[0]}</Text>
-                                                        <Text as="p">{src.summary}</Text>
-                                                    </Flex>
-                                                    <HoverCard.Arrow className="HoverCardArrow" />
-                                                </HoverCard.Content>
-                                            </HoverCard.Portal>
-                                        </HoverCard.Root>
-                                    </Flex>
-                                    <Flex className="justify-center pb-2">
+                                <div className="embla__slide px-2" key={i}>
+                                    <HoverCard.Root>
+                                        <HoverCard.Trigger asChild>
+                                            <div id="image" className="relative">
+                                                <Image
+                                                    src={src.coverurl}
+                                                    width="300"
+                                                    height="0"
+                                                    className="ImageTrigger p-2 border-2 border-slate-300 bg-slate-50"
+                                                    alt="alt"
+                                                />
+                                            </div>
+                                        </HoverCard.Trigger>
+                                        <HoverCard.Portal>
+                                            <HoverCard.Content className="HoverCardContent text-xs md:text-sm overflow-hidden overflow-y-auto data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all" sideOffset={5} side="left">
+                                                <Flex grow="1" direction="column" gap="3">
+                                                    <Text color="ruby" className="text-lg">{splitTitle[0]}</Text>
+                                                    <Text as="p">{src.summary}</Text>
+                                                </Flex>
+                                                <HoverCard.Arrow className="HoverCardArrow" />
+                                            </HoverCard.Content>
+                                        </HoverCard.Portal>
+                                    </HoverCard.Root>
+                                    <Flex id="bib" className="justify-center pb-2">
                                         <Flex grow="1" direction="column" className="justify-center text-slate-700">
                                             <a href={`https://onesearch.library.northeastern.edu/permalink/01NEU_INST/lt8evo/alma${src.mmsId}`} target="_blank" className="text-center">
                                                 <Text size="4" className="text-center line-clamp-1">{splitTitle[0]}</Text>
@@ -67,7 +65,7 @@ export const Newbooks = ({ data }: {data: any}) => {
                                             <Text className="text-xs md:text-sm lg-text-lg text-center">Received: {src.recDate}</Text>
                                         </Flex>
                                     </Flex>
-                                </Flex>
+                                </div>
                             );
                         })}
                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,38 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridAutoColumns: {
+        '100': '100%',
+        '33': '33%',
+        '20': '20%',
+        '14': '14.25%',
+      },
+      keyframes: {
+        slideUpAndFade: {
+          '0%': { opacity: 0, transform: 'translateY(2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideRightAndFade: {
+          '0%': { opacity: 0, transform: 'translateX(-2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+        slideDownAndFade: {
+          '0%': { opacity: 0, transform: 'translateY(-2px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+        slideLeftAndFade: {
+          '0%': { opacity: 0, transform: 'translateX(2px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+      animation: {
+        slideUpAndFade: 'slideUpAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+        slideDownAndFade: 'slideDownAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+        slideRightAndFade: 'slideRightAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+        slideLeftAndFade: 'slideLeftAndFade 300ms cubic-bezier(0.16, 0, 0.13, 1)',
+      },
+    },
   },
   plugins: [],
 }
