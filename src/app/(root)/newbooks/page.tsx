fc37607ecb9c9b7ab36c02b7fbc206000367db88
John Winsor <johnwinsor@gmail.com>
2024-01-29 21:24:08 -0800

    fixed next rewrites

--- src/app/(root)/newbooks/page.tsx
+++ src/app/(root)/newbooks/page.tsx
@@ -4,7 +4,8 @@ import { Flex, Text} from '@radix-ui/themes';
 export default async function newBooksPage() {
     
     const res = await fetch(
-        'http://localhost:3000/api/newbooks',
+        // 'http://localhost:3000/api/newbooks',
+        'http://https://oakland-library.vercel.app/api/newbooks',
         { cache: 'no-store' },
     );
     const data = await res.json()
