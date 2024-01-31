e78487afb069dab0e1ed7f0828f01bb0900d758b
John Winsor <johnwinsor@gmail.com>
2024-01-31 12:36:35 -0800

    added route for all books data api

--- api/index.py
+++ api/index.py
@@ -5,19 +5,23 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-response = requests.get('https://library.mills.edu/data.json')
-data = response.json()
+# response = requests.get('https://library.mills.edu/data.json')
+# data = response.json()
 
 @app.route("/api/newbooks", methods=["GET"])
 def get_all_todo_items():
+    response = requests.get('https://library.mills.edu/data.json')
+    data = response.json()
     return data
 
 @app.route("/api/newbooks/<subj>", methods=["GET"])
 def get_todo_item(subj):
-    todo = ([book for book in data if book["subject"] == subj], None)
-    if todo:
-        return todo
-    return {"error": "Todo item not found"}, 404
+    response = requests.get('https://library.mills.edu/data-all.json')
+    data = response.json()
+    books = ([book for book in data if book["subject"] == subj], None)
+    if books:
+        return books
+    return {"error": "Books item not found"}, 404
 
 @app.route("/api/healthchecker", methods=["GET"])
 def healthchecker():
