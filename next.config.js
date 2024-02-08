37ffa80c5ec755e3cdcae00953774feeea50399e
John Winsor <johnwinsor@gmail.com>
2024-02-08 15:17:44 -0800

    path

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
         destination: 
         process.env.NODE_ENV === 'development'
           ? 'http://127.0.0.1:8080/api/:path*'
-          : '/api/',
+          : 'http://127.0.0.1:8080/api/:path*',
     },
     ]
   },  
