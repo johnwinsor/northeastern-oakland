a2d40eef5c786228eb2a41769f5fd5aaafe24eef
John Winsor <johnwinsor@gmail.com>
2024-02-08 15:39:25 -0800

    using HOST_BASE_URL

--- src/app/(root)/newbooks/[subject]/page.tsx
+++ src/app/(root)/newbooks/[subject]/page.tsx
@@ -4,8 +4,9 @@ import SelectSubjects from '@/components/selectSubjects'
 
 
 export default async function newBooksSubjectPage({ params }: { params: { subject: string } }) {
+    
     const res = await fetch(
-        `${process.env.VERCEL_URL}/api/newbooks/${params.subject}`,
+        `${process.env.HOST_BASE_URL}/api/newbooks/${params.subject}`,
         { cache: 'no-store' },
     );
     const data = await res.json()
