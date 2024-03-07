3b3dfda6f9a10e2a5ce91bdd84ed8d1bbcde494f
John Winsor <johnwinsor@gmail.com>
2024-03-07 09:02:33 -0800

    turned off image optimization for to avoid Vercel limits

--- next.config.js
+++ next.config.js
@@ -1,44 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  rewrites: async () => {
-    return [
-    {
-        source: '/api/:path*',
-        destination: 
-        process.env.NODE_ENV === 'development'
-          ? 'http://127.0.0.1:8080/api/:path*'
-          : 'http://127.0.0.1:8080/api/:path*',
-    },
-    ]
-  },  
-  images: {
+    rewrites: async () => {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 
+                process.env.NODE_ENV === 'development'
+                ? 'http://127.0.0.1:8080/api/:path*'
+                : 'http://127.0.0.1:8080/api/:path*',
+            },
+        ]
+    },  
+    images: {
+        unoptimized: true,    
         remotePatterns: [
-          {
+            {
             protocol: 'https',
             hostname: 'covers.openlibrary.org',
             port: '',
             pathname: '/b/**',
-          },
-          {
+            },
+            {
             protocol: 'https',
             hostname: 'books.google.com',
             port: '',
             pathname: '/books/**',
-          },
-          {
+            },
+            {
             protocol: 'https',
             hostname: 'm.media-amazon.com',
             port: '',
             pathname: '/books/**',
-          },
-          {
+            },
+            {
             protocol: 'https',
             hostname: 'tailwindui.com',
             port: '',
             pathname: '/img/**',
-          },
+            },
         ],
-      }
+    }
 }
 
 module.exports = nextConfig
