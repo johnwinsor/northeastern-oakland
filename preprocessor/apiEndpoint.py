be93fce491fd5541e8190642222f634878bd9173
John Winsor <johnwinsor@gmail.com>
2024-04-19 17:08:09 -0700

    full database

--- preprocessor/apiEndpoint.py
+++ preprocessor/apiEndpoint.py
@@ -11,11 +11,14 @@ import env
 from pathlib import Path
 import logging
 from datetime import datetime
+from tqdm import tqdm
+from time import sleep
 
 now = datetime.now()
 logging.basicConfig(filename='logs/api.log', filemode='a', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
-logging.info("++++++++++++++++++++++++++++++++++++")
+logging.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 logging.info(f"START OF LOG FOR {now}")
+logging.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 
 googleKey = env.googleKey
 almaKey = env.almaKey
@@ -150,16 +153,14 @@ def getGoogleCover(googleBook):
         return None
 
 def getAnalyticsJson(latestPOL):
-
     # Process Delta
-    almaUrl = f"https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?path=%2Fshared%2FNortheastern%20University%2FJohnShared%2FAPI%2FAcq-Analysis&limit={records}&apikey={almaKey}&filter=%3Csawx%3Aexpr%20xsi%3Atype%3D%22sawx%3Acomparison%22%20op%3D%22greater%22%0A%20%20xmlns%3Asaw%3D%22com.siebel.analytics.web%2Freport%2Fv1.1%22%20%0A%20%20xmlns%3Asawx%3D%22com.siebel.analytics.web%2Fexpression%2Fv1.1%22%20%0A%20%20xmlns%3Axsi%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema-instance%22%20%0A%20%20xmlns%3Axsd%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%22%0A%3E%0A%20%20%3Csawx%3Aexpr%20xsi%3Atype%3D%22sawx%3AsqlExpression%22%3E%22Funds%20Expenditure%22.%22PO%20Line%20Reference%22%3C%2Fsawx%3Aexpr%3E%0A%20%20%3Csawx%3Aexpr%20xsi%3Atype%3D%22xsd%3Astring%22%3E{latestPOL}%3C%2Fsawx%3Aexpr%3E%0A%3C%2Fsawx%3Aexpr%3E"
+    #almaUrl = f"https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?path=%2Fshared%2FNortheastern%20University%2FJohnShared%2FAPI%2FAcq-Analysis&limit={records}&apikey={almaKey}&filter=%3Csawx%3Aexpr%20xsi%3Atype%3D%22sawx%3Acomparison%22%20op%3D%22greater%22%0A%20%20xmlns%3Asaw%3D%22com.siebel.analytics.web%2Freport%2Fv1.1%22%20%0A%20%20xmlns%3Asawx%3D%22com.siebel.analytics.web%2Fexpression%2Fv1.1%22%20%0A%20%20xmlns%3Axsi%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema-instance%22%20%0A%20%20xmlns%3Axsd%3D%22http%3A%2F%2Fwww.w3.org%2F2001%2FXMLSchema%22%0A%3E%0A%20%20%3Csawx%3Aexpr%20xsi%3Atype%3D%22sawx%3AsqlExpression%22%3E%22Funds%20Expenditure%22.%22PO%20Line%20Reference%22%3C%2Fsawx%3Aexpr%3E%0A%20%20%3Csawx%3Aexpr%20xsi%3Atype%3D%22xsd%3Astring%22%3E{latestPOL}%3C%2Fsawx%3Aexpr%3E%0A%3C%2Fsawx%3Aexpr%3E"
     
     # Process Full Dataset
-    # almaUrl = f"https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?path=%2Fshared%2FNortheastern%20University%2FJohnShared%2FAPI%2FAcq-Analysis&limit={records}&apikey={almaKey}"
+    almaUrl = f"https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?path=%2Fshared%2FNortheastern%20University%2FJohnShared%2FAPI%2FAcq-Analysis&limit={records}&apikey={almaKey}"
     booksJson = []
     IsFinished = False
     while not IsFinished:
-        print(f"Getting {almaUrl}")
         response = requests.get(almaUrl)
         if response.status_code == 200:
             my_dict = xmltodict.parse(response.content)
@@ -209,15 +210,21 @@ def getAnalyticsJson(latestPOL):
                     booksJson.append(book)
             except KeyError:
                 print("No new records")
+                return booksJson
             Finished = my_dict['report']['QueryResult']['IsFinished']
             if(Finished == "true"):
                 IsFinished = True
-            print(f"Finished: {IsFinished}")
-            try:
-                ResumptionToken = my_dict['report']['QueryResult']['ResumptionToken']
-            except:
-                print(f"Using existing resumption token: {ResumptionToken}")
-            almaUrl = f"https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?token={ResumptionToken}&limit=25&apikey=l8xx5852c9867ab64264901d17af13574837"
+                print("Finished getting Analytics data")
+                print("-------------------------------")
+            else:
+                try:
+                    ResumptionToken = my_dict['report']['QueryResult']['ResumptionToken']
+                    print(f"Found resumption token. Paginating...")
+                except:
+                    print(f"Paginating...")
+                almaUrl = f"https://api-na.hosted.exlibrisgroup.com/almaws/v1/analytics/reports?token={ResumptionToken}&limit=25&apikey=l8xx5852c9867ab64264901d17af13574837"
+        else:
+            print(f"ERROR GETTING ANALYTICS API - {almaUrl}")
     return booksJson
             
 def main():
@@ -226,42 +233,59 @@ def main():
     latestPOL = f.read()
     print(f"Latest POL: {latestPOL}")
     
+    print(f"Getting Analytics Report Data after {latestPOL}...")
     analyticsJson = getAnalyticsJson(latestPOL)
-    lastPOL = analyticsJson[0]['POL']
-    analyticsDataLength = len(analyticsJson)
-    print(f"Found {analyticsDataLength} records in Analytics report")
-    newBooks = []
-    missingCovers = []
-    counter = 1
-    hits = 0
-    misses = 0
-    dupes = 0
-    nr = 0
-    with open('../api/static/newbooks.json', 'r') as seenBooksJson:
-        seenBooks = json.load(seenBooksJson)
-        dataLength = len(seenBooks)
-        print(f"Found {dataLength} seen records")
-        for book in analyticsJson:
-            logging.info("------------------------------------")
-            logging.info(counter)
+    if analyticsJson:
+        newLatestPOL = analyticsJson[0]['POL']
+        analyticsDataLength = len(analyticsJson)
+        print(f"Attempting to match {analyticsDataLength} new titles from Analytics report with enhanced metadata.")
+        newBooks = []
+        counter = 0
+        hits = 0
+        misses = 0
+        dupes = 0
+        nr = 0
+        
+        print(f"Opening Existing Dataset...")
+        with open('../api/static/newbooks.json', 'r') as seenBooksJson:
+            seenBooks = json.load(seenBooksJson)
+            
+        seenDataCount = len(seenBooks)
+        print(f"Found {seenDataCount} seen records")
+        
+        with open('notReceivedBooks.json', 'r') as startNotReceivedBooksJson:
+            startNotReceivedBooks = json.load(startNotReceivedBooksJson)
+        startNotReceivedBooksCount = len(startNotReceivedBooks)
+            
+        with open('missingCovers.json', 'r') as startMissingCoversJson:
+            startMissingCovers = json.load(startMissingCoversJson)
+        startMissingCoversCount = len(startMissingCovers)
+        
+        print("Starting matching process...")
+        for book in tqdm(analyticsJson):
+            sleep(0.5)
             counter+=1
-            mmsId = book['mmsId']
-            logging.info(mmsId)
-            if any(d.get('mmsId') == mmsId for d in seenBooks):
-                logging.info(book['Title'])
+            logging.WARNING("------------------------------------------------------------")
+            logging.CRITICAL(counter)
+            pol = book['POL']
+            logging.info(pol)
+            logging.info(book['Title'])
+            if any(d.get('POL') == pol for d in seenBooks):
                 logging.info("FOUND BOOK in SEEN DATA")
                 dupes += 1
                 continue
             if book['SortDate'] == '0000-00-00':
-                logging.info(book['Title'])
-                logging.info("Book not received")
+                logging.info("BOOK NOT YET RECEIVED")
                 nr += 1
+                if any(d.get('POL') == pol for d in startNotReceivedBooks):
+                    logging.info("BOOK ALREADY IN NOT RECEIVED FILE")
+                else:
+                    logging.info("APPENDING BOOK TO NOT RECEIVED FILE")
+                    startNotReceivedBooks.append(book)
                 continue
             try:
                 isbn13 = book['isbn13']
                 logging.info(isbn13)
-                logging.info(book['Title'])
-                time.sleep(1)
                 googleBook = getGoogleBook(book['isbn13'])
                 try:
                     googleItem = googleBook['items'][0]
@@ -286,8 +310,12 @@ def main():
                             else:
                                 logging.info("No suitable Open Library book cover found")
                                 logging.info("Skipping title")
-                                missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'No suitable book cover found'}
-                                missingCovers.append(missingbook)
+                                # missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'No suitable book cover found'}
+                                if any(d.get('POL') == pol for d in startMissingCovers):
+                                    logging.info("BOOK ALREADY IN NOT MISSING COVERS FILE")
+                                else:
+                                    logging.info("ADDING BOOK MISSING COVERS FILE")
+                                    startMissingCovers.append(book)
                                 misses += 1
                                 continue      
                     else:
@@ -299,8 +327,12 @@ def main():
                         else:
                             logging.info(f"No suitable Open Library book cover found")
                             logging.info("Skipping title")
-                            missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'No suitable book cover found'}
-                            missingCovers.append(missingbook)
+                            # missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'No suitable book cover found'}
+                            if any(d.get('POL') == pol for d in startMissingCovers):
+                                logging.info("BOOK ALREADY IN NOT MISSING COVERS FILE")
+                            else:
+                                logging.info("ADDING BOOK MISSING COVERS FILE")
+                                startMissingCovers.append(book)
                             misses += 1
                             continue
                     hits += 1
@@ -308,74 +340,85 @@ def main():
                 except:
                     logging.info('NO GOOGLE METADATA')
                     logging.info("Skipping title")
-                    missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'NO GOOGLE METADATA'}
-                    missingCovers.append(missingbook)
+                    # missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'NO GOOGLE METADATA'}
+                    if any(d.get('POL') == pol for d in startMissingCovers):
+                        logging.info("BOOK ALREADY IN NOT MISSING COVERS FILE")
+                    else:
+                        logging.info("ADDING BOOK MISSING COVERS FILE")
+                        startMissingCovers.append(book)
                     misses += 1
                     continue
             except:
                 logging.info("No ISBN13")
-                logging.info(book['Title'])
-                missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'No ISBN13'}
-                missingCovers.append(missingbook)
+                # missingbook = {'mmsId': book['mmsId'], 'isbn13': book['isbn13'], 'SortDate': '9999-99-99', 'error': 'No ISBN13'}
+                if any(d.get('POL') == pol for d in startMissingCovers):
+                    logging.info("BOOK ALREADY IN NOT MISSING COVERS FILE")
+                else:
+                    logging.info("ADDING BOOK TO MISSING COVERS FILE")
+                    startMissingCovers.append(book)
                 misses += 1
                 logging.info("Skipping title")
-    
+
         cleanNewBooks = replace_null_with_empty_string(newBooks)
-        filteredBooks = [fb for fb in cleanNewBooks if fb['SortDate'] != '0000-00-00']
+        newBooksCount = len(newBooks)
         
-        for b in filteredBooks:
+        print(f"Found {dupes} titles already in Dataset...")
+        print(f"Matched {newBooksCount} titles with usable covers. Appending to Dataset...")
+        
+        for b in cleanNewBooks:
             seenBooks.append(b)
             
+        print(f"Sorting Dataset...")
         sortedSeenBooks = sorted(seenBooks, key=operator.itemgetter('SortDate'), reverse=True)
         sortedSeenBooksCount = len(sortedSeenBooks)
-        with open('newbooks.json', "w") as nb:
+
+        print(f"Writing New Dataset to tempNewbooks.json...")
+        with open('newbooksTemp.json', "w") as nb:
             json.dump(sortedSeenBooks, nb, indent=4)
         
-        sortedNewBooks = sorted(filteredBooks, key=operator.itemgetter('SortDate'), reverse=True)
+        sortedNewBooks = sorted(cleanNewBooks, key=operator.itemgetter('SortDate'), reverse=True)
         sortedNewBooksCount = len(sortedNewBooks)
-        with open('newFoundBooks.json', "w") as nb:
-            json.dump(sortedNewBooks, nb, indent=4)
         
-        with open('missingCovers.json', "r") as mbr:
-            file_data = json.load(mbr)
-            for new in missingCovers:
-                file_data.append(new)
+        print(f"Writing {newBooksCount} New Books Matched to newFoundBooks.json...")
+        with open('newFoundBooks.json', "w") as f:
+            json.dump(sortedNewBooks, f, indent=4)
         
-        with open('missingCovers.json', "w") as mbw:
-            json.dump(file_data, mbw, indent=4)
+        endMissingCoversCount = len(startMissingCovers)
+        print(f"Writing {endMissingCoversCount} Unmatched Books to missingCovers.json...")
+        with open('missingCovers.json', "w") as f:
+            json.dump(startMissingCovers, f, indent=4)
+        print(f"Missing Covers books changed from {startMissingCoversCount} to {endMissingCoversCount}")
+            
+        endNotReceivedBooksCount = len(startNotReceivedBooks)
+        print(f"Writing {endNotReceivedBooksCount} Non-Received Books to notReceivedBooks.json...")
+        with open('notReceivedBooks.json', "w") as f:
+            json.dump(startNotReceivedBooks, f, indent=4)
+        print(f"Not received books changed from {startNotReceivedBooksCount} to {endNotReceivedBooksCount}")
         
-        print("------------------------------------")
-        print(f"Found {dataLength} records in existing dataset")
-        print(f"Attempted to match {analyticsDataLength} new titles from Analytics report with enhanced metadata.")
-        print(f"Found {dupes} titles in existing dataset")
-        print(f"Matched {sortedNewBooksCount} titles with usable covers. Adding to Dataset and writing to newbooks.json.")
-        print(f"New Dataset size: {sortedSeenBooksCount}")
-        print(f"{nr} titles not yet received and skipped")
-        print(f"Unable to match {misses} titles with usable covers")
         
         logging.info("------------------------------------")
-        logging.info(f"Found {dataLength} records in existing dataset")
-        logging.info(f"Attempted to match {analyticsDataLength} new titles from Analytics report with enhanced metadata.")
         logging.info(f"Found {dupes} titles in existing dataset")
         logging.info(f"Matched {sortedNewBooksCount} titles with usable covers. Adding to Dataset and writing to newbooks.json.")
         logging.info(f"New Dataset size: {sortedSeenBooksCount}")
         logging.info(f"{nr} titles not yet received and skipped")
         logging.info(f"Unable to match {misses} titles with usable covers")
+            
+        print(f"Moving new dataset to API directory...")
+        currentPath = Path.cwd()
+        source = currentPath / f"newbooksTemp.json"
+        dest = currentPath.parent / f"api/static/"
+        oldfile = dest / f"newbooks.json"
+        oldfile.replace(f"{dest}/newbooks.json.bak")
+        source.replace(f"{dest}/newbooks.json")
+        print("DONE")
+        print(f"Last POL Processed: {newLatestPOL}. Writing to lastRecord.txt")
         
-    print(f"Moving new dataset to API directory...")
-    currentPath = Path.cwd()
-    source = currentPath / f"newbooks.json"
-    dest = currentPath.parent / f"api/static/"
-    oldfile = dest / f"newbooks.json"
-    oldfile.replace(f"{dest}/newbooks.json.bak")
-    source.replace(f"{dest}/newbooks.json")
-    print("DONE")
-    print(f"Last POL Processed: {lastPOL}. Writing to lastRecord.txt")
-    
-    f = open("lastRecord.txt", "w")
-    f.write(lastPOL)
-    f.close()
-    print("DONE")
+        f = open("lastRecord.txt", "w")
+        f.write(newLatestPOL)
+        f.close()
+        print("DONE")
+    else:
+        print("DONE")
 
 if __name__ == "__main__":
     main()
